e3c298a3266c3e19c25fe48be7a0e837e077b014
Sanskar Dwivedi <72437494+Knighthawk-Leo@users.noreply.github.com>
2021-06-30 12:09:56 +0530

    Create Input and Output.cpp

---
+++ C++/Input and Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a1,a2,a3,sum;  //declaring variables
+    
+    cin>>a1>>a2>>a3 ;   //taking input
+    sum=a1+a2+a3;
+    cout<<sum;
+    return 0;
+}
